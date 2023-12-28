56a688966e08090e3bb426e3b4f8b4920445a494
NeverMit <markovnikov96@mail.ru>
2023-12-28 18:47:58 +0500

    Использовали метод join(). Убрали метод sleep()

--- src/main/java/potoki/SomeTask.java
+++ src/main/java/potoki/SomeTask.java
@@ -9,28 +9,28 @@ public class SomeTask {
     private static final int TO_NUMBER_SECOND_THREAD=1000;
     private static final String TEMPLATE_MESSAGE_THREAD_NAME_AND_NUMBER="%s : %d\n";
 
-    private static final int TIME_WAITING_IN_MILLIS=1000;
+
 
     public static void main(String[] args) throws InterruptedException {
-        final TaskSummingNumbers firstTask=startSubTask(FROM_NUMBER_FIRST_THREAD,TO_NUMBER_FIRST_THREAD);
+        final TaskSummingNumbers firstTask=new TaskSummingNumbers(FROM_NUMBER_FIRST_THREAD,TO_NUMBER_FIRST_THREAD);
+        final Thread firstThread=new Thread(firstTask);
+        firstThread.start();
 
-        final TaskSummingNumbers secondTask=startSubTask(FROM_NUMBER_SECOND_THREAD,TO_NUMBER_SECOND_THREAD);
+        final TaskSummingNumbers secondTask=new TaskSummingNumbers(FROM_NUMBER_SECOND_THREAD,TO_NUMBER_SECOND_THREAD);
+        final Thread secondThread=new Thread(secondTask);
+        secondThread.start();
 
-        waitForTasksFinished();
+        waitForTasksFinished(firstThread,secondThread);
 
         final int resultNumber= firstTask.getResultNumber()+secondTask.getResultNumber();
 
         printThreadNameAndNumber(resultNumber);
     }
-    private static void waitForTasksFinished() throws InterruptedException{
-        Thread.sleep(TIME_WAITING_IN_MILLIS);
+    private static void waitForTasksFinished(final Thread... threads) throws InterruptedException{
+       for(final Thread thread:threads)
+           thread.join();
     }
-    private static TaskSummingNumbers startSubTask(final int fromNumber,final int toNumber){
-        final TaskSummingNumbers subTask=new TaskSummingNumbers(fromNumber,toNumber);
-        final Thread thread=new Thread(subTask);
-        thread.start();
-        return subTask;
-    }
+
     private static void printThreadNameAndNumber(final int number){
         System.out.printf(
                 TEMPLATE_MESSAGE_THREAD_NAME_AND_NUMBER,
