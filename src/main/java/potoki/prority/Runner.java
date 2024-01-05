7c5a334c37c5140892cf5cca279c96400eb64b6a
NeverMit <markovnikov96@mail.ru>
2024-01-05 16:11:27 +0500

    Разбираем приоритеты потоков

---
+++ src/main/java/potoki/prority/Runner.java
@@ -0,0 +1,23 @@
+package potoki.prority;
+
+import java.util.stream.IntStream;
+
+public class Runner {
+    private static final String MESSAGE_MAIN_THREAD_FINISHED="Main thread is finished";
+
+    public static void main(String[] args) {
+        final Thread thread=new Thread(new Task());
+        thread.setPriority(Thread.MAX_PRIORITY);
+        thread.start();
+        System.out.println(MESSAGE_MAIN_THREAD_FINISHED);
+    }
+
+    private static final class Task implements Runnable{
+        private static final int RANGE_MINIMAL_BORDER=0;
+        private static final int RANGE_MAXIMAL_BORDER=100;
+        @Override
+        public void run(){
+            IntStream.range(RANGE_MINIMAL_BORDER,RANGE_MAXIMAL_BORDER).forEach(System.out::println);
+        }
+    }
+}
