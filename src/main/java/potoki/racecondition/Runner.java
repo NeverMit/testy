fa0b4180cbd4a8dcc09328c764d4269c06ffa111
NeverMit <markovnikov96@mail.ru>
2024-01-09 16:39:20 +0500

    Разбираем состояние гонок и атомарные операции

---
+++ src/main/java/potoki/racecondition/Runner.java
@@ -0,0 +1,28 @@
+package potoki.racecondition;
+
+import static java.util.stream.IntStream.range;
+
+public class Runner {
+    private static int counter=0;
+
+    private static final int INCREMENT_AMOUNT_FIRST_THREAD=500;
+    private static final int INCREMENT_AMOUNT_SECOND_THREAD=600;
+
+    public static void main(String[] args) throws InterruptedException {
+        final Thread firstThread=createIncrementingCounterThread(INCREMENT_AMOUNT_FIRST_THREAD);
+        final Thread secondThread=createIncrementingCounterThread(INCREMENT_AMOUNT_SECOND_THREAD);
+
+        firstThread.start();
+        secondThread.start();
+
+        firstThread.join();
+        secondThread.join();
+
+        System.out.println(counter);
+    }
+    private static Thread createIncrementingCounterThread(final int incrementAmount){
+        return new Thread(()->
+                range(0,incrementAmount).forEach(i->counter++)
+        );
+    }
+}
